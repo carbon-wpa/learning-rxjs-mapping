3060dcc0765205d7d185be70578319b231d0255f
Kacper Slotwinski <kacper.slotwinski@gmail.com>
2021-02-15 08:20:32 +0100

    map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  constructor(public http: HttpClient) {
+  }
+
+  public onBtnClick(): void {
+    this.http.get('https://60295804289eb50017cf796d.mockapi.io/testData')
+      .pipe(
+        map((data: any) => data.map(item => item.name))
+      )
+      .subscribe(
+        console.log
+      );
+  }
 }
